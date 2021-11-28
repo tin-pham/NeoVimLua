dc8e99e3426cd11b6aae93b00d8fec646738635e
shin <heoconlinhat123@gmail.com>
2021-11-28 13:59:45 +0700

    Migrate property

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -1,58 +1,46 @@
 require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = true,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = false,
+  update_to_buf_dir = {enable = true, auto_open = true},
   diagnostics = {
     enable = true,
     icons = {
       hint = "",
       info = "",
       warning = "",
-      error = "",
+      error = ""
     }
   },
   update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
+    enable = false,
+    update_cwd = false,
     ignore_list = {}
   },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
-  },
+  system_open = {cmd = nil, args = {}},
+  filters = {dotfiles = false, custom = {}},
+  git = {enable = true, ignore = true, timeout = 500},
   view = {
     width = 25,
     height = 30,
     hide_root_folder = false,
     side = 'left',
     auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
+    mappings = {custom_only = false, list = {}}
   }
 }
 
-vim.g.nvim_tree_indent_markers = 1 
+vim.g.nvim_tree_indent_markers = 1
 -- Fix lag
 vim.g.nvim_tree_show_icons = {
   git = 0,
   folders = 1, -- or 0,
   files = 1, -- or 0,
-  folder_arrows = 0, -- or 0
+  folder_arrows = 0 -- or 0
 }
-vim.g.nvim_tree_gitignore = 0
 vim.g.nvim_tree_git_hl = 0
