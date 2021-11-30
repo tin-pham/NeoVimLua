45bbbf9841f674038cba2767162dd306d5947811
shin <heoconlinhat123@gmail.com>
2021-12-01 05:43:10 +0700

    Remove query execute on save

--- lua/dadbod-config/init.lua
+++ lua/dadbod-config/init.lua
@@ -4,3 +4,4 @@ vim.g.db_ui_auto_execute_table_helpers = 0
 vim.g.db_ui_winwidth = 20
 vim.g.db_ui_save_location = '~/GGDrive/shin/db_ui_queries'
 vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_execute_on_save = 0
