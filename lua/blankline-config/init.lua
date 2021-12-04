e61ff8109e11afb7295d68cb6fc7a2139001fb77
shin <heoconlinhat123@gmail.com>
2021-12-04 20:40:11 +0700

    nothing new

--- lua/blankline-config/init.lua
+++ lua/blankline-config/init.lua
@@ -1,11 +1,14 @@
 require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    buftype_exclude = {"terminal"},
-    filetype_exclude = {"dashboard", "NvimTree", "packer"},
-    show_current_context = true,
-    show_current_context_start = true,
-    context_patterns = {
-        "class", "return", "function", "method", "^if", "^while", "jsx_element", "^for", "^object", "^table", "block", "arguments", "if_statement",
-        "else_clause", "jsx_element", "jsx_self_closing_element", "try_statement", "catch_clause", "import_statement", "operation_type"
-    }
+  buftype_exclude = {"terminal"},
+  filetype_exclude = {"dashboard", "NvimTree", "packer"},
+  show_current_context = true,
+  context_patterns = {
+    "class", "return", "function", "method", "^if", "^while",
+    "jsx_element", "^for", "^object", "^table", "block", "arguments",
+    "if_statement", "else_clause", "jsx_element",
+    "jsx_self_closing_element", "try_statement", "catch_clause",
+    "import_statement", "operation_type"
   }
+}
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=none gui=nocombine]]
