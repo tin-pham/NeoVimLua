eb8bc4f4246bdbfc24f2a6ca14310e3bfdb87870
shin <heoconlinhat123@gmail.com>
2021-12-04 20:40:38 +0700

    Change dashboard header

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -1,9 +1,44 @@
-vim.g.dashboard_default_executive ='telescope'
-
-vim.g.dashboard_custom_section = {
-    a = {description = {'  Find File          '}, command = 'Telescope find_files'},
-    d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
-    b = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-    e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'},
+local g = vim.g
+g.dashboard_disable_statusline = 0
+vim.cmd(
+    'autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2')
+g.dashboard_custom_header = {
+  "                                   ",
+  "                                   ",
+  "                                   ",
+  "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+  "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+  "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+  "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+  "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+  "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+  "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+  " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+  " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+  "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+  "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+  "                                   "
 }
-vim.g.dashboard_custom_footer = {'Có làm thì mới có ăn - Huấn Hoa Hồng'}
+g.dashboard_default_executive = 'telescope'
+g.dashboard_custom_section = {
+  a = {
+    description = {'  Find File          '},
+    command = 'Telescope find_files'
+  },
+  d = {
+    description = {'  Search Text        '},
+    command = 'Telescope live_grep'
+  },
+  b = {
+    description = {'  Recent Files       '},
+    command = 'Telescope oldfiles'
+  },
+  e = {
+    description = {'  Config             '},
+    command = 'edit ~/.config/nvim/init.lua'
+  }
+}
+g.dashboard_custom_footer = {
+  'Có làm thì mới có ăn - Huấn Hoa Hồng'
+}
+
