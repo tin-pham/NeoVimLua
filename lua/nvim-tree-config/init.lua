617ce84297ad84ac7a12e2dace7bbc4ba7b53482
shin <heoconlinhat123@gmail.com>
2021-12-04 20:40:55 +0700

    Customize and optimize nvim-tree-config

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -1,15 +1,25 @@
+local g = vim.g
+vim.o.termguicolors = true
+
+-- shorten root folder string
+g.nvim_tree_root_folder_modifier = table.concat {
+  ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"
+}
+-- Fix lag
+
+g.nvim_tree_git_hl = 1
+g.nvim_tree_add_trailing = 0
+g.nvim_tree_highlight_opened_files = 0
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1, -- or 0,
+  files = 1, -- or 0,
+  folder_arrows = 0 -- or 0
+}
 require'nvim-tree'.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_close = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = false,
-  update_to_buf_dir = {enable = true, auto_open = true},
   diagnostics = {
-    enable = true,
+    enable = false,
     icons = {
       hint = "",
       info = "",
@@ -17,30 +27,40 @@ require'nvim-tree'.setup {
       error = ""
     }
   },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {}
+  filters = {dotfiles = false},
+  disable_netrw = true,
+  hijack_netrw = true,
+  ignore_ft_on_setup = {"dashboard"},
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = true,
+  update_cwd = true,
+  update_focused_file = {enable = true, update_cwd = false},
+  view = {allow_resize = true, side = "left", width = 25},
+
+  git = {enable = true, ignore = true, timeout = 500}
+}
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    deleted = "",
+    ignored = "◌",
+    renamed = "➜",
+    staged = "✓",
+    unmerged = "",
+    unstaged = "✗",
+    untracked = "★"
   },
-  system_open = {cmd = nil, args = {}},
-  filters = {dotfiles = false, custom = {}},
-  git = {enable = true, ignore = true, timeout = 500},
-  view = {
-    width = 25,
-    height = 30,
-    hide_root_folder = false,
-    side = 'left',
-    auto_resize = false,
-    mappings = {custom_only = false, list = {}}
+  folder = {
+    -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
+    -- arrow_open = "",
+    -- arrow_closed = "",
+    default = "",
+    empty = "", -- 
+    empty_open = "",
+    open = "",
+    symlink = "",
+    symlink_open = ""
   }
 }
-
-vim.g.nvim_tree_indent_markers = 1
--- Fix lag
-vim.g.nvim_tree_show_icons = {
-  git = 0,
-  folders = 1, -- or 0,
-  files = 1, -- or 0,
-  folder_arrows = 0 -- or 0
-}
-vim.g.nvim_tree_git_hl = 0
