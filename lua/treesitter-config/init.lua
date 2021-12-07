a8958f460dd890010f035aa1aa1b8dd0afdca958
shin <heoconlinhat123@gmail.com>
2021-12-07 19:10:12 +0700

    Add autotag when press <CR> in JSX

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,36 +1,35 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"html", "javascript", "lua"}, 
+  ensure_installed = {
+    "html", "css", "scss", "json", "javascript", "lua", "vim"
+  },
   highlight = {
-    enable = true,              
-    additional_vim_regex_highlighting = false,
+    enable = true,
+    additional_vim_regex_highlighting = false
   },
 
   -- Auto Tag Plugin
-  autotag = {
-    enable = true,
-  },
+  autotag = {enable = true},
 
   -- Rainbow Plugin
   rainbow = {
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  -- Auto Indent when <CR> 
+    max_file_lines = nil -- Do not enable for files with more than n lines, int
+    -- Auto Indent when <CR> 
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
-  }
+  },
+  indent = {enable = true}
 }
 
 -- LSP fix
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
-        underline = true,
-        virtual_text = {
-            spacing = 5,
-            severity_limit = 'Warning',
-        },
-        update_in_insert = true,
-    }
-)
+                                                          vim.lsp
+                                                              .diagnostic
+                                                              .on_publish_diagnostics,
+                                                          {
+      underline = true,
+      virtual_text = {spacing = 5, severity_limit = 'Warning'},
+      update_in_insert = true
+    })
