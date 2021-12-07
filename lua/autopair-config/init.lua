254d794778ff79a48ed1f6cd507ab7ca1361d30c
shin <heoconlinhat123@gmail.com>
2021-12-07 19:07:22 +0700

    Add space between parenthesis

--- lua/autopair-config/init.lua
+++ lua/autopair-config/init.lua
@@ -1,16 +1,39 @@
-require('nvim-autopairs').setup{
- 
+require('nvim-autopairs').setup {
 
   -- { |} <CR> =>  {
-    --                 |
-    --             }    
-  
-    check_ts = true,
+  --                 |
+  --             }    
+
+  check_ts = true
 
 }
 
-  -- Auto Indent when <CR> 
+-- Add space between parenthesis
+local npairs = require 'nvim-autopairs'
+local Rule = require 'nvim-autopairs.rule'
+
+npairs.add_rules {
+  Rule(' ', ' '):with_pair(function(opts)
+    local pair = opts.line:sub(opts.col - 1, opts.col)
+    return vim.tbl_contains({'()', '[]', '{}'}, pair)
+  end), Rule('( ', ' )'):with_pair(function()
+    return false
+  end):with_move(function(opts)
+    return opts.prev_char:match('.%)') ~= nil
+  end):use_key(')'), Rule('{ ', ' }'):with_pair(function()
+    return false
+  end):with_move(function(opts)
+    return opts.prev_char:match('.%}') ~= nil
+  end):use_key('}'), Rule('[ ', ' ]'):with_pair(function()
+    return false
+  end):with_move(function(opts)
+    return opts.prev_char:match('.%]') ~= nil
+  end):use_key(']')
+}
+
+-- Auto Indent when <CR> 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+cmp.event:on('confirm_done',
+             cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
 
