05d02e18b517e771149ffdb4edf94936d98e3282
shin <heoconlinhat123@gmail.com>
2021-12-18 06:36:14 +0700

    Decrease the height of zenmode

--- lua/zenmode-config/init.lua
+++ lua/zenmode-config/init.lua
@@ -2,7 +2,8 @@ require("zen-mode").setup {
   window = {
     backdrop = 0.90,
     width = .85, -- width will be 85% of the editor width
-    height = 1
+    height = 0.95
 
-  }
+  },
+  plugins = {gitsigns = {enabled = false}}
 }
