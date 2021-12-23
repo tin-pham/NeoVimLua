18c9d20f761bc13572d80b25a144b534ea7dd63d
shin <heoconlinhat123@gmail.com>
2021-12-24 05:53:43 +0700

    Add scss

--- lua/format-config/init.lua
+++ lua/format-config/init.lua
@@ -2,6 +2,7 @@ require'format'.setup {
   html = {{cmd = {"prettier -w"}}},
   ejs = {{cmd = "prettier -w"}},
   css = {{cmd = {"prettier -w"}}},
+  scss = {{cmd = {"prettier -w"}}},
   json = {{cmd = {"prettier -w"}}},
   javascript = {
     {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
