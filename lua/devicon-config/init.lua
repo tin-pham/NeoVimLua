02ff417087e1b7d27cb4c895f0304409d346d2ca
shin <heoconlinhat123@gmail.com>
2022-02-15 11:36:35 +0700

    Add more iocn

--- lua/devicon-config/init.lua
+++ lua/devicon-config/init.lua
@@ -1,6 +1,7 @@
 local green = '#87af87'
 local darkYellow = '#d1bd74'
 local blue = '#01579b'
+local lightBlue = '#84a9ac'
 local pink = '#cc6699'
 local red = '#cb3837'
 
@@ -10,6 +11,8 @@ require("nvim-web-devicons").set_icon {
   css = {icon = "", color = blue, name = 'css'},
   scss = {icon = "", color = pink, name = 'sass'},
   html = {icon = "", color = red, name = 'html'},
-  http = {icon = "", color = green, name = 'http'}
+  http = {icon = "", color = green, name = 'http'},
+  txt = {icon = "", color = lightBlue, name = 'txt'},
+  md = {icon = "", color = lightBlue, name = 'markdown'}
 }
 
