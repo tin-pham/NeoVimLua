1dcfd4fffafa172991ddab66ba9eaf0db5bcef1a
shin <heoconlinhat123@gmail.com>
2022-07-19 07:58:37 +0700

    Comment in JSX/TSX

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,27 +1,26 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then return end
+
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "html", "css", "scss", "json", "javascript", "lua", "vim", "dot",
-    "http", "regex", "jsdoc", "comment", "markdown", "query"
-  },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
+  ensure_installed = "all",
+  ignore_install = {""}, -- List of parsers to ignore installing
+  highlight = {enable = true, additional_vim_regex_highlighting = false},
 
   -- Auto Tag Plugin
   autotag = {enable = true},
 
   -- Rainbow Plugin
   rainbow = {
-    enable = true,
+    enable = false,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil -- Do not enable for files with more than n lines, int
-    -- Auto Indent when <CR> 
+    -- Auto Indent when <CR>
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   },
   indent = {enable = true},
+  context_commentstring = {enable = true, enable_autocmd = false},
 
   textobjects = {
     select = {
@@ -43,12 +42,3 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- LSP fix
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-                                                          vim.lsp
-                                                              .diagnostic
-                                                              .on_publish_diagnostics,
-                                                          {
-      underline = true,
-      virtual_text = {spacing = 5, severity_limit = 'Warning'},
-      update_in_insert = true
-    })
