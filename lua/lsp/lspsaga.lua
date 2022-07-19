edc43e5355d08e1240f7433ac9040907a2adada7
shin <heoconlinhat123@gmail.com>
2022-07-20 05:42:55 +0700

    update saga

--- lua/lsp/lspsaga.lua
+++ lua/lsp/lspsaga.lua
@@ -1,7 +1,6 @@
 local lspsaga = require 'lspsaga'
-lspsaga.setup { -- defaults ...
-  debug = false,
-  use_saga_diagnostic_sign = true,
+lspsaga.init_lsp_saga({ -- defaults ...
+  border_style = 'single',
   -- diagnostic sign
   error_sign = "",
   warn_sign = "",
@@ -18,18 +17,22 @@ lspsaga.setup { -- defaults ...
     open = "o",
     vsplit = "s",
     split = "i",
+    tabe = "t",
     quit = "q",
     scroll_down = "<C-f>",
-    scroll_up = "<C-b>"
+    scroll_up = "<C-b>" -- quit can be a table
   },
+
   code_action_keys = {quit = "q", exec = "<CR>"},
   rename_action_keys = {quit = "<C-c>", exec = "<CR>"},
+
   definition_preview_icon = "  ",
-  border_style = "single",
   rename_prompt_prefix = "➤",
+
   rename_output_qflist = {enable = false, auto_open_qflist = false},
   server_filetype_map = {},
   diagnostic_prefix_format = "%d. ",
   diagnostic_message_format = "%m %c",
   highlight_prefix = false
-}
+
+})
