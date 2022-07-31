4727723a93091989201540d887dbcf850e71a954
shin <heoconlinhat123@gmail.com>
2022-07-31 14:25:53 +0700

    remove comment

--- init.lua
+++ init.lua
@@ -3,15 +3,5 @@ require('options')
 require('keybindings')
 require('themes')
 
-require('treesitter-config') -- Highlight Syntax -- TODO: Lazy Loading  
+require('treesitter-config') -- Highlight Syntax -- TODO: Lazy Loading
 require('dashboard-config') -- TODO: Lazy Loading
-
--- require('lualine-config') -- Status Line
--- require('format-config')
--- require('null-ls-config')
--- require('commentstring-config')
--- require('surround-config')
--- require('blankline-config')
--- require('doge-config')
--- require('rest-config')
--- require('colorizer-config')
